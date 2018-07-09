d8e78e6a751c14468f4a39a04d6eca23e5f211d3
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-07-09 10:42:47 +0300

    Rename test

--- tests/test_wfdb_dataset.py
+++ tests/test_wfdb_dataset.py
@@ -1,10 +1,8 @@
 import os
-import re
 import unittest
 
 import wfdb
 
-from ecgbc.dataset import ECG_CHANNEL_PATTERN
 from ecgbc.dataset.wfdb_dataset import WFDBDataset
 from tests import TEST_RESOURCES_PATH
 
@@ -86,38 +84,6 @@ class WFDBDatasetCustomPattern2Test(WFDBDatasetTestBase, unittest.TestCase):
             self.assertEqual(sample.sig_name[0], 'MLII')
             self.assertTrue(sample.sig_name[1] == 'V1'or
                             sample.sig_name[1] == 'V5')
-
-
-class ECGChannelRegexTest(unittest.TestCase):
-    def setUp(self):
-        self.pattern = re.compile(ECG_CHANNEL_PATTERN, re.IGNORECASE)
-
-        self.positive_test_cases = [
-            'ecg', 'foo ECG', 'ECG_bar', 'ECG1', 'ecg2', 'foo ecg3', 'foo_ecg',
-            'MLI', 'MLII', 'MLIII', 'foo MLI', 'MLIIII bar',
-            'V5', 'foo v1', 'v4 bar',
-            'lead ii', 'foo lead iii', 'lead I bar', 'ECG Lead II',
-            'iiii', 'foo II', 'II bar',
-        ]
-
-        self.negative_test_cases = [
-            'foobar', 'foo bar baz', 'foo_bar', 'foo1', 'foo 1', '2 bar',
-            '2_bar', 'foo foo bar', 'bar_foo_baz', 'IIfoo', 'fooIII',
-            'wrist_ppg', 'PPG', 'resp', 'ART', 'PAP', 'CVP', 'resp imp.',
-            'CO2',
-        ]
-
-    def test_positive_cases(self):
-        for teststr in self.positive_test_cases:
-            for teststr_case in [teststr, teststr.upper(), teststr.lower()]:
-                match = self.pattern.search(teststr_case)
-                self.assertIsNotNone(match, msg=teststr_case)
-
-    def test_negative_cases(self):
-        for teststr in self.negative_test_cases:
-            for teststr_case in [teststr, teststr.upper(), teststr.lower()]:
-                match = self.pattern.search(teststr_case)
-                self.assertIsNone(match, msg=teststr_case)
 
 
 if __name__ == '__main__':
