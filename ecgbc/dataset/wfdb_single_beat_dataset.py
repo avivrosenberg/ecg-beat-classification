355d6a5acdc64b14ba98bcf0cf34c43e5cab9fde
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-07-16 16:09:29 +0300

    WFDBSingleBeatDataset: RR interval features calculation

--- ecgbc/dataset/wfdb_single_beat_dataset.py
+++ ecgbc/dataset/wfdb_single_beat_dataset.py
@@ -2,6 +2,7 @@ import math
 
 import numpy as np
 import scipy.interpolate as interp
+import scipy.signal as signal
 import os.path
 import re
 import subprocess
@@ -18,13 +19,16 @@ class WFDBSingleBeatDataset(Dataset):
     ECGPUWAVE_BIN = 'bin/ecgpuwave-1.3.3/ecgpuwave'
     DEFAULT_RESAMPLE_DURATION_S = 0.8
     DEFAULT_RESAMPLE_NUM_SAMPLES = 50
+    RR_MAX = 1.5
+    RR_MIN = 0.4
 
     def __init__(self, wfdb_dataset, in_ann_ext='atr', out_ann_ext='ecgatr',
                  resample_duration_s=DEFAULT_RESAMPLE_DURATION_S,
                  resample_num_samples=DEFAULT_RESAMPLE_NUM_SAMPLES,
-                 calculate_rr_features=True, ecgpuwave_bin=ECGPUWAVE_BIN):
+                 calculate_rr_features=True, filter_rri=False,
+                 ecgpuwave_bin=ECGPUWAVE_BIN):
         """
-
+        A dataset of single ECG beats extracted from WFDB records.
         :type wfdb_dataset: WFDBDataset
         :param wfdb_dataset: A dataset of WFDB records.
         :param in_ann_ext: File extension of annotator containing beat types.
@@ -36,6 +40,7 @@ class WFDBSingleBeatDataset(Dataset):
         self.resample_duration_s = resample_duration_s
         self.resample_num_samples = resample_num_samples
         self.calculate_rr_features = calculate_rr_features
+        self.filter_rri = filter_rri
         self.ecgpuwave_bin = ecgpuwave_bin
 
         # Compile once to reduce per-record overhead
@@ -87,7 +92,89 @@ class WFDBSingleBeatDataset(Dataset):
 
             beat_segments[:, i] = seg
 
+        if self.calculate_rr_features:
+            r_peak_times = np.array(r_peaks) / record.fs
+
+            rri, rrt, filter_idx = self.rr_intervals(r_peak_times)
+            beat_segments = beat_segments[:, filter_idx]
+
+            rr_features, filter_idx = self.rri_features(rri, rrt)
+            beat_segments = beat_segments[:, filter_idx]
+
+            beat_segments = np.vstack((
+                beat_segments,
+                rr_features
+            ))
+
         return beat_segments
+
+    def rr_intervals(self, r_peak_times):
+        rri = np.diff(r_peak_times)  # Prepend zero interval
+        rrt = np.r_[0, np.cumsum(rri[:-1])]
+
+        # Filter out non-physiological intervals
+        if self.filter_rri:
+            filter_idx = (rri < self.RR_MAX) & (rri > self.RR_MIN)
+            rri = rri[filter_idx]
+            rrt = rrt[filter_idx]
+        else:
+            filter_idx = np.full_like(rri, True, dtype=bool)
+
+        # Prepend false because the first peak in r_peak_times has no interval
+        filter_idx = np.r_[False, filter_idx]
+
+        return rri, rrt, filter_idx
+
+    def rri_features(self, rri, rrt):
+        rr_features_next = rri
+        rr_features_prev = np.r_[0, rri[:-1]]
+
+        rri_features = np.vstack((
+            rr_features_next,
+            rr_features_prev,
+        ))
+
+        # Resample the RR intervals with uniform rate so we can apply filters
+        fs_resampled = 4  # Hz
+        rrt_rs = np.r_[0:rrt[-1]:1/fs_resampled]
+        rri_rs = interp.interp1d(rrt, rri)(rrt_rs)
+
+        def sliding_window_mean(sig, m):
+            filter_kernel = np.ones((m,))
+            filter_kernel /= m
+            result = signal.convolve(sig, filter_kernel, mode='full')
+            # Define N as the signal length and M as the filter length.
+            # Taking the first N values from the 'full' convolution means were
+            # doing a moving average that looks at the past. Taking the last N
+            # would be like looking at the future. Taking the 'middle' would be
+            # like a centered moving average.
+            # result = result[m-1:]  # looking forward
+            result = result[0:len(rri_rs)]  # looking back
+            return result
+
+        sliding_win_durations_s = [10, 5 * 60]
+        for sliding_window_s in sliding_win_durations_s:
+            filter_len_samples = sliding_window_s * fs_resampled
+            rr_intervals_mean = sliding_window_mean(rri_rs, filter_len_samples)
+            rr_intervals_mean_at = interp.interp1d(
+                rrt_rs, rr_intervals_mean, fill_value='extrapolate'
+            )
+
+            # Stack the filter result in the features matrix
+            rri_features = np.vstack((
+                rri_features,
+                # interpolate filter results at rrt
+                rr_intervals_mean_at(rrt)
+            ))
+
+        # Remove features up to time of the longest filter, because only then
+        # is the filter performing a valid calculation
+        filter_idx = rrt > np.max(sliding_win_durations_s)
+        rrt = rrt[filter_idx]
+        rri = rri[filter_idx]
+        rri_features = rri_features[:, filter_idx]
+
+        return rri_features, filter_idx
 
     def resample_segment(self, record, segment_idx):
         """
