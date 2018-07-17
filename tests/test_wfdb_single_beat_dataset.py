6a2871ff09f50e4fb6b5701231722347cea9395d
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-07-17 10:19:24 +0300

    tests for WFDBSingleBeatDataset

---
+++ tests/test_wfdb_single_beat_dataset.py
@@ -0,0 +1,78 @@
+import os
+import pathlib
+import unittest
+from pathlib import Path
+
+import wfdb
+
+from ecgbc.dataset.wfdb_dataset import WFDBDataset
+from ecgbc.dataset.wfdb_single_beat_dataset import WFDBSingleBeatDataset
+from tests import TEST_RESOURCES_PATH
+
+WFDB_TEST_RESOURCES_PATH = f'{TEST_RESOURCES_PATH}/wfdb'
+WFDB_NUMBER_OF_TEST_RESOURCES = 2
+
+
+class WFDBSingleBeatDatasetTest(unittest.TestCase):
+
+    def setUp(self):
+        self.wfdb_dataset = WFDBDataset(
+            WFDB_TEST_RESOURCES_PATH, first_channel_only=True
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        # Delete previously-created annotations
+        for ann_file_path in Path(WFDB_TEST_RESOURCES_PATH).glob("*.ecgatr"):
+            os.remove(ann_file_path)
+
+    def test_feature_vector_length_with_rr_features(self):
+        # Arrange
+        resample_num_samples = 42
+        wfdb_single_beat_dataset = WFDBSingleBeatDataset(
+            self.wfdb_dataset,
+            resample_num_samples=resample_num_samples,
+            calculate_rr_features=True)
+
+        expected_feature_vector_length = resample_num_samples + 4
+        self._test_feature_vector_length_helper(wfdb_single_beat_dataset,
+                                                expected_feature_vector_length)
+
+    def test_feature_vector_length_without_rr_features(self):
+        # Arrange
+        resample_num_samples = 97
+        wfdb_single_beat_dataset = WFDBSingleBeatDataset(
+            self.wfdb_dataset,
+            resample_num_samples=resample_num_samples,
+            calculate_rr_features=False)
+
+        expected_feature_vector_length = resample_num_samples + 0
+        self._test_feature_vector_length_helper(wfdb_single_beat_dataset,
+                                                expected_feature_vector_length)
+
+    def _test_feature_vector_length_helper(self,
+                                           wfdb_single_beat_dataset,
+                                           expected_feature_vector_length):
+        # Act
+        for segments, labels in wfdb_single_beat_dataset:
+            # Assert
+            actual_feature_vector_length = segments.shape[0]
+            self.assertEqual(expected_feature_vector_length,
+                             actual_feature_vector_length)
+
+    def test_labels_match_segments(self):
+        # Arrange
+        wfdb_single_beat_dataset = WFDBSingleBeatDataset(self.wfdb_dataset)
+
+        # Act
+        for segments, labels in wfdb_single_beat_dataset:
+            num_labels = len(labels)
+            num_segments = segments.shape[1]
+
+            # Assert
+            self.assertEqual(num_labels, num_segments)
+            self.assertEqual((num_labels,), labels.shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
