5fc8702b1eae4d46bfd6b333ed9ca78b0db48fd3
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-07-19 16:16:47 +0300

    main: Initial entrypoint script
    Currently provides functionality to generate a single-beat dataset.

---
+++ main.py
@@ -0,0 +1,65 @@
+import argparse
+import os
+
+import ecgbc.dataset.transforms
+import ecgbc.dataset.wfdb_dataset
+import torchvision.transforms
+
+import ecgbc.dataset.wfdb_single_beat
+
+
+def parse_cli():
+    def is_dir(dirname):
+        if not os.path.isdir(dirname):
+            raise argparse.ArgumentTypeError(f'{dirname} is not a directory')
+        else:
+            return dirname
+
+    parser = argparse.ArgumentParser(
+        prog='ecgbc', description='ECG single beat classification'
+    )
+    subparsers = parser.add_subparsers(help='Sub-command help')
+
+    # Generate dataset
+    subparser_generate = subparsers.add_parser(
+        'generate-dataset', help='Generate a single-beat dataset')
+    subparser_generate.set_defaults(subcmd_fn=generate_dataset)
+    subparser_generate.add_argument('--in', '-i', type=is_dir, dest='in_dir',
+                                    help='Input dir', required=True)
+    subparser_generate.add_argument('--out', '-o', type=str, dest='out_dir',
+                                    help='Output dir', required=True)
+    subparser_generate.add_argument('--transform-lpf', action='store_true',
+                                    help='Apply lowpass-filter')
+    subparser_generate.add_argument('--transform-sma', action='store_true',
+                                    help='Subtract moving average')
+    subparser_generate.add_argument('--ann-ext', '-a', type=str, default='atr',
+                                    help='Input annotation extension')
+
+    return parser.parse_args()
+
+
+def generate_dataset(in_dir, out_dir, ann_ext, **kwargs):
+    """
+    Generates a single-beat dataset based on a folder containing WFDB records.
+    """
+    transforms = []
+    if kwargs['transform_lpf']:
+        transforms.append(ecgbc.dataset.transforms.LowPassFilterWFDB())
+    if kwargs['transform_sma']:
+        transforms.append(ecgbc.dataset.transforms.SubtractMovingAverageWFDB())
+
+    wfdb_dataset = ecgbc.dataset.wfdb_dataset.WFDBDataset(
+        root_path=in_dir,
+        transform=torchvision.transforms.Compose(transforms))
+
+    generator = ecgbc.dataset.wfdb_single_beat.Generator(
+        wfdb_dataset, in_ann_ext=ann_ext, out_ann_ext=f'ecg{ann_ext}',
+        calculate_rr_features=True, filter_rri=False
+    )
+
+    generator.write(out_dir)
+
+
+if __name__ == '__main__':
+    parsed_args = parse_cli()
+    parsed_args.subcmd_fn(**vars(parsed_args))
