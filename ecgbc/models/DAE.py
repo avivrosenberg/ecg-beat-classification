04d7d4b205f59e9af5f15c408ec230145fa8a0aa
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-08-01 17:08:11 +0300

    DAE model: Allow encoder-decoder weight sharing

--- ecgbc/models/DAE.py
+++ ecgbc/models/DAE.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as nnfunc
 
 
 class Encoder(nn.Module):
@@ -7,27 +8,55 @@ class Encoder(nn.Module):
 
         layers = []
         prev_layer_size = input_feature_size
-        for curr_layer_size in hidden_layer_sizes:
-            layers += [
-                nn.Linear(prev_layer_size, curr_layer_size, bias=True),
-                nn.Sigmoid()
-            ]
+        for i, curr_layer_size in enumerate(hidden_layer_sizes):
+            self.add_module(
+                self._layer_name(i),
+                nn.Sequential(
+                    nn.Linear(prev_layer_size, curr_layer_size, bias=True),
+                    nn.Sigmoid()
+                ))
             prev_layer_size = curr_layer_size
 
-        self.encoder = nn.Sequential(*layers)
+        self.num_layers = len(hidden_layer_sizes)
 
     def forward(self, x):
-        return self.encoder(x)
+        y = x
+        for layer_idx in range(self.num_layers):
+            layer = getattr(self, self._layer_name(layer_idx))
+            y = layer(y)
+        return y
+
+    def get_layer_weights(self, layer_idx):
+        if layer_idx not in range(self.num_layers):
+            raise ValueError("Invalid layer index")
+
+        layer = getattr(self, self._layer_name(layer_idx))
+        layer_params = list(layer.parameters())
+
+        # First parameter is weight, second is bias
+        return layer_params[0]
+
+    def _layer_name(self, layer_idx):
+        return f'layer_{layer_idx}'
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, ext_weights=None):
         super(Decoder, self).__init__()
 
         self.decoder = nn.Linear(input_size, output_size, bias=True)
 
+        if ext_weights is not None:
+            assert ext_weights.shape[0] == output_size
+            assert ext_weights.shape[1] == input_size
+            self.decoder.weight = None
+            self.ext_weights = ext_weights
+
     def forward(self, x):
-        return self.decoder(x)
+        if self.ext_weights is not None:
+            return nnfunc.linear(x, self.ext_weights, self.decoder.bias.data)
+        else:
+            return self.decoder(x)
 
 
 class Classifier(nn.Module):
