335569a9ff28dda510e4ab0b541a4c04241ae3a9
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-08-05 22:54:10 +0300

    pytorch_tools: Trainer now includes loss function

--- pytorch_tools/trainer.py
+++ pytorch_tools/trainer.py
@@ -3,30 +3,48 @@ import math
 import time
 
 import torch
-import torch.utils.data.dataloader
+import torch.utils.data.dataloader as tdl
 
 
 class ModelTrainer(abc.ABC):
     """
     Class containing boilerplate code for training pytorch models.
-    A trainer consists of a Model, and an Optimizer and a set of Hyperparameter
-    values.
+    A trainer consists of a Model, a Loss function, an Optimizer and a
+    set of Hyperparameter values.
     """
 
-    def __init__(self):
+    def __init__(self, **hyperparams):
+        # Set hyperparams as attributes of the trainer
+        for hp in hyperparams:
+            setattr(self, hp, hyperparams[hp])
+
+        # Construct the modules
         self.model = self.create_model()
         self.optimizer = self.create_optimizer()
+        self.loss_fn = self.create_loss()
 
-    def train(self, dl_train: torch.utils.data.dataloader.DataLoader,
-              verbose=False):
+    def train(self, dl_train: tdl.DataLoader, verbose=False):
         self.model.train()
         return _foreach_dataloader(dl_train, self.train_batch, verbose)
 
-    def test(self, dl_test: torch.utils.data.dataloader.DataLoader,
-             verbose=False):
+    def test(self, dl_test: tdl.DataLoader, verbose=False):
         self.model.eval()
         with torch.autograd.no_grad():
             return _foreach_dataloader(dl_test, self.test_batch, verbose)
+
+    def fit(self, dl_train, dl_test, num_epochs, verbose=False):
+        results = []
+
+        for epoch in range(num_epochs):
+            if verbose:
+                print(f'*** EPOCH {epoch+1}/{num_epochs} ***')
+
+            train_loss = self.train(dl_train, verbose)
+            test_loss = self.test(dl_test, verbose)
+
+            results.append(dict(train_loss=train_loss.item(),
+                                test_loss=test_loss.item()))
+        return results
 
     @abc.abstractmethod
     def create_model(self) -> torch.nn.Module:
@@ -34,6 +52,10 @@ class ModelTrainer(abc.ABC):
 
     @abc.abstractmethod
     def create_optimizer(self) -> torch.optim.Optimizer:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def create_loss(self) -> torch.nn.Module:
         raise NotImplementedError()
 
     @abc.abstractmethod
@@ -45,8 +67,7 @@ class ModelTrainer(abc.ABC):
         raise NotImplementedError()
 
 
-def _foreach_dataloader(loader: torch.utils.data.dataloader.DataLoader,
-                        forward_fn, verbose=False):
+def _foreach_dataloader(loader: tdl.DataLoader, forward_fn, verbose=False):
     avg_loss = torch.tensor(0.0)
     num_batches = math.ceil(len(loader.dataset) / loader.batch_size)
 
