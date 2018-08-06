4d94e5ec67a94f14c22270bc2af9aaf06f76e6ea
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-08-06 16:11:17 +0300

    Trainer: Improve results handling
    Refactored the way training results are returned.
    Added accuracy to the results.

--- pytorch_tools/trainer.py
+++ pytorch_tools/trainer.py
@@ -1,10 +1,30 @@
 import abc
 import math
-import time
 
 import torch
 import torch.utils.data.dataloader as tdl
 import tqdm
+
+from typing import Callable, Any, NamedTuple, List, Dict
+
+
+class BatchResult(NamedTuple):
+    loss: float
+    num_correct: int
+
+
+class EpochResult(NamedTuple):
+    avg_loss: float
+    accuracy: float
+
+
+class FitResult(NamedTuple):
+    num_epochs: int
+    hyperparams: Dict[str, Any]
+    train_loss: List[float]
+    train_acc: List[float]
+    test_loss: List[float]
+    test_acc: List[float]
 
 
 class ModelTrainer(abc.ABC):
@@ -24,31 +44,36 @@ class ModelTrainer(abc.ABC):
         self.optimizer = self.create_optimizer()
         self.loss_fn = self.create_loss()
 
-    def train(self, dl_train: tdl.DataLoader, verbose=False):
+    def train(self, dl_train: tdl.DataLoader, verbose=False) -> EpochResult:
         self.model.train()
         progress_bar = 'Train' if verbose else None
-        return _foreach_dataloader(dl_train, self.train_batch, progress_bar)
+        return _foreach_batch(dl_train, self.train_batch, progress_bar)
 
-    def test(self, dl_test: tdl.DataLoader, verbose=False):
+    def test(self, dl_test: tdl.DataLoader, verbose=False) -> EpochResult:
         self.model.eval()
         progress_bar = 'Test' if verbose else None
         with torch.autograd.no_grad():
-            return _foreach_dataloader(dl_test, self.test_batch, progress_bar)
+            return _foreach_batch(dl_test, self.test_batch, progress_bar)
 
-    def fit(self, dl_train, dl_test, num_epochs, verbose=False):
-        results = []
+    def fit(self, dl_train, dl_test, num_epochs, verbose=False) -> FitResult:
+
+        train_loss, train_acc, test_loss, test_acc = [], [], [], []
+        result = FitResult(num_epochs, self.hyperparams(), train_loss,
+                           train_acc, test_loss, test_acc)
 
         for epoch in range(num_epochs):
             if verbose:
                 print(f'*** EPOCH {epoch+1}/{num_epochs} ***')
 
-            train_loss = self.train(dl_train, verbose)
-            test_loss = self.test(dl_test, verbose)
+            train_result = self.train(dl_train, verbose)
+            test_result = self.test(dl_test, verbose)
 
-            results.append(dict(epoch=epoch,
-                                train_loss=train_loss.item(),
-                                test_loss=test_loss.item()))
-        return results
+            train_loss.append(train_result.avg_loss)
+            train_acc.append(train_result.accuracy)
+            test_loss.append(test_result.avg_loss)
+            test_acc.append(test_result.accuracy)
+
+        return result
 
     @abc.abstractmethod
     def create_model(self) -> torch.nn.Module:
@@ -63,28 +88,62 @@ class ModelTrainer(abc.ABC):
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def train_batch(self, dl_sample) -> torch.Tensor:
+    def train_batch(self, dl_batch) -> BatchResult:
+        """
+        Runs a single batch forward through the model, calculates loss,
+        preforms back-propagation and uses the optimizer to update weights.
+        :param dl_batch: A single batch of data  from a data loader (might
+            be a tuple of data and labels or anything else depending on
+            the underlying dataset.
+        :return: A BatchResult containing the value of the loss function and
+            the number of correctly classified samples in the batch.
+        """
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def test_batch(self, dl_sample) -> torch.Tensor:
+    def test_batch(self, dl_batch) -> BatchResult:
+        """
+        Runs a single batch forward through the model and calculates loss.
+        :param dl_batch: A single batch of data  from a data loader (might
+            be a tuple of data and labels or anything else depending on
+            the underlying dataset.
+        :return: A BatchResult containing the value of the loss function and
+            the number of correctly classified samples in the batch.
+        """
         raise NotImplementedError()
 
+    def hyperparams(self):
+        hypers = {}
 
-def _foreach_dataloader(loader: tdl.DataLoader, forward_fn, progress=None):
-    avg_loss = torch.tensor(0.0)
-    num_batches = math.ceil(len(loader.dataset) / loader.batch_size)
+        non_hyper_attrs = ('model', 'optimizer', 'loss_fn')
+        for attr in dir(self):
+            if attr.startswith('_') or attr in non_hyper_attrs:
+                continue
+            attr_value = getattr(self, attr)
+            if callable(attr_value):
+                continue
+            hypers[attr] = attr_value
+        return hypers
 
+
+def _foreach_batch(loader: tdl.DataLoader,
+                   forward_fn: Callable[[Any], BatchResult],
+                   progress: str = None) -> EpochResult:
     pbar = None
+    total_loss = 0.0
+    num_correct = 0
+
+    num_samples = len(loader.dataset)
+    num_batches = math.ceil(num_samples / loader.batch_size)
 
     def process_item(i, d):
         return forward_fn(d)
 
     def process_item_with_pbar(i, d):
-        loss = forward_fn(d)
-        pbar.set_description(f'{progress} ({loss.item():.3f})')
+        bres = forward_fn(d)
+        pbar.set_description(f'{progress} ({bres.loss:.3f})')
         pbar.update()
-        return loss
+        return bres
 
     if progress is not None:
         total = math.ceil(len(loader.dataset) / loader.batch_size)
@@ -94,11 +153,16 @@ def _foreach_dataloader(loader: tdl.DataLoader, forward_fn, progress=None):
         process_fn = process_item
 
     for batch_idx, data in enumerate(loader):
-        avg_loss += process_fn(batch_idx, data)
+        batch_result = process_fn(batch_idx, data)
+        total_loss += batch_result.loss
+        num_correct += batch_result.num_correct
 
-    avg_loss /= num_batches
+    avg_loss = total_loss / num_batches
+    accuracy = 100. * num_correct / num_samples
 
     if pbar:
-        pbar.set_description(f'{progress} (Avg. {avg_loss.item():.3f})')
+        pbar.set_description(f'{progress} '
+                             f'(Avg. Loss {avg_loss:.3f}, '
+                             f'Accuracy {accuracy:.1f})')
 
-    return avg_loss
+    return EpochResult(avg_loss=avg_loss, accuracy=accuracy)
